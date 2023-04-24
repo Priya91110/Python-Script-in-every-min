c107fc08076c255f0db195da9b2cc333d36c0b05
Priyanka <priyankacarpenter30@gmail.com>
2023-04-24 12:09:42 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+
+# write a python script to print hello world in every minute on github action 
+
+import time
+
+while True:
+    print("Hello, World!")
+    time.sleep(60)  # Wait for 60 seconds
+
